ef51633f02efa490755d1d0b5e9b55e81ce922ec
Adracir <ricardaboente@t-online.de>
2022-10-14 15:09:56 +0200

    add plan for crawler to generate more corpora

---
+++ crawler.py
@@ -0,0 +1,5 @@
+# TODO: implement web crawler to generate fictional and speech text corpora
+#   tutorial: https://www.blog.datahut.co/post/how-to-build-a-web-crawler-from-scratch (uses scrapy)
+#   pages:
+#       speeches: https://ec.europa.eu/info/strategy/eu-budget/long-term-eu-budget/2021-2027/negotiations/speeches_en
+#       literature: https://gutenberg.org/
