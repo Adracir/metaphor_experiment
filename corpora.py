8714d8e535078aef9c6a3e198192b4af50143101
Adracir <ricardaboente@t-online.de>
2022-10-15 15:46:41 +0200

    continue setting up cleaning method for wiki dump

--- corpora.py
+++ corpora.py
@@ -26,25 +26,59 @@ def preprocess_wiki_dump():
         if not re.match('#REDIRECT', p_text):
             # removes &quot; (quotation mark)
             p_text = re.sub('&quot;', '', p_text)
-            # removes stuff inside {{}} (references, meta)
-            # TODO: solve problem that 18 {{ remain because they were nested
-            p_text = re.sub('{{.[^{{]*}}', '', p_text)
+            # removes stuff inside {{}} (references, meta) &  inside {||} (tables), also if nested
+            p_text = remove_text_inside_brackets(p_text, brackets="{}")
+            # removes stuff inside <ref> and </ref> (some references)
+            p_text = re.sub('(<ref.*?>.*?</ref>|<ref.*?/>)', '', p_text)
+            # removes stuff inside <div> (html stylings) (also nested)
+
+            # removes '' (marks text as italic) and ''' (marks text as bold)
+            p_text = re.sub('(\'){2,3}', '', p_text)
+            # removes stuff inside [[Category: ]] (links to wiki categories)
+            p_text = re.sub('\[\[Category\:.*?\]\]', '', p_text)
+            # TODO: remove stuff inside [[File:]] (image metadata & image description)
+            #  and also take care of nested brackets! maybe rewrite remove_text_inside_brackets for it!
             cleaned_texts.append(p_text)
 
     print(f'text length after: {len(cleaned_texts)}')
     print(f'cleaned texts: {cleaned_texts}')
+    return ""
+
+
+def remove_text_inside_brackets(text, brackets):
+    # for brackets containing of multiple characters, treated as regex
+    if len(brackets[0]) > 1:
+        groups = ['']
+        for m in re.finditer(brackets[0]):
+            if i == brackets[0]:
+                groups.append(i)
+            elif i == brackets[1] and len(groups) > 1:
+                groups.pop()
+            else:
+                groups[-1] += i
+        return "".join(groups)
+    else:
+        groups = ['']
+        # iterates through text, character by character
+        for i in text:
+            # if character indicates opening bracket, add to group
+            if i == brackets[0]:
+                groups.append(i)
+            # if character indicates closing bracket, and there is something contained in group,
+            # remove last item from group
+            elif i == brackets[1] and len(groups) > 1:
+                groups.pop()
+            # if it is any other character, add this to the end of the group
+            else:
+                groups[-1] += i
+        return "".join(groups)
+
 
     # TODO:
     #   remove
-    #       '' (marks text as italic)
-    #       ''' (marks text as bold)
-    #       stuff inside {| (tables)
-    #       stuff inside &lt;ref and &gt; (some references, if all starting from &lt;, also mathematical formulas etc
-    #        might be excluded)
+    #       maybe mathematical formulas
+    #       [ ] (links, but contains also other info)
     #       [[ ]] (link to other wiki page)
     #           if two alternatives marked by |, use second (indicating visible text)
-    #           remove whole content inside those brackets if it begins with "File:" (image metadata & image
-    #           description)
-    return ""
 
 preprocess_wiki_dump()
